ada0054ac549b4d0035ba7272fb11fe0f3296cab
Breno Cardoso <elisio.cardoso@dimensa.com.br>
2022-12-29 17:41:37 -0300

    dash-3: Assets for Datatable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,17 @@
 
 //AdminLTE App
 //= require dist/js/adminlte.min.js
+
+//Datatable Responsive
+//= require plugins/datatables/jquery.dataTables.min.js
+//= require plugins/datatables-bs4/js/dataTables.bootstrap4.min.js
+//= require plugins/datatables-responsive/js/dataTables.responsive.min.js
+//= require plugins/datatables-responsive/js/responsive.bootstrap4.min.js
+//= require plugins/datatables-buttons/js/dataTables.buttons.min.js
+//= require plugins/datatables-buttons/js/buttons.bootstrap4.min.js
+
+//= require plugins/datatables-buttons/js/buttons.html5.min.js
+//= require plugins/datatables-buttons/js/buttons.print.min.js
+//= require plugins/datatables-buttons/js/buttons.colVis.min.js
+
+
