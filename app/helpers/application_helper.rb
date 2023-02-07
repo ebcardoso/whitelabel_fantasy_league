4c6d2961a09867f300a38bd200fdac0bc03a1c30
Breno Cardoso <elisiobreno@gmail.com>
2023-02-07 00:10:06 -0300

    dash-29: refactor sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def sidebar_menus
+    [
+      {
+        title: 'Fantasy Leagues',
+        icon: 'fa-tachometer-alt',
+        menus: [
+          {
+            title: 'Index',
+            path: fantasy_leagues_url
+          },
+        ]
+      },
+    ]
+  end
 end
